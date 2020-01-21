c8e250c88e8b919a3dae478ac75bc75098f98d3e
leocamargoslemes@hotmail.com <32150838leozin>
2020-01-21 16:14:55 -0300

    Curso SeleniumWebDriver

--- src/test/java/Tests/ResumoTest.java
+++ src/test/java/Tests/ResumoTest.java
@@ -2,22 +2,29 @@ package Tests;
 
 import Core.BasePage;
 import Core.BaseTest;
+import Core.Driverfactory;
 import Pages.MenuPage;
 import Pages.ResumoPage;
 import org.junit.Assert;
 import org.junit.FixMethodOrder;
 import org.junit.Test;
 import org.junit.runners.MethodSorters;
+import org.openqa.selenium.By;
+import org.openqa.selenium.NoSuchElementException;
+import org.openqa.selenium.WebElement;
+
+import java.sql.Driver;
+import java.util.List;
 
 import static Core.Driverfactory.getDriver;
 
 @FixMethodOrder(MethodSorters.NAME_ASCENDING)
 public class ResumoTest extends BaseTest {
-private MenuPage menuPage = new MenuPage();
-private ResumoPage resumoPage = new ResumoPage();
+    private MenuPage menuPage = new MenuPage();
+    private ResumoPage resumoPage = new ResumoPage();
 
     @Test
-    public void test1ExcluirMovimentacao(){
+    public void test1ExcluirMovimentacao() {
         menuPage.acessarTelaResumo();
 
         resumoPage.excluiMovimentacao();
@@ -27,11 +34,24 @@ private ResumoPage resumoPage = new ResumoPage();
     }
 
     @Test
-    public void test2ResumoMensal(){
+    public void test2ResumoMensal() {
+
         menuPage.acessarTelaResumo();
 
         Assert.assertEquals("Seu Barriga - Extrato", getDriver().getTitle());
+
+//		DriverFactory.getDriver().findElement(By.xpath("//*[@id='tabelaExtrato']/tbody/tr"));
+
+//		try{
+//			DriverFactory.getDriver().findElement(By.xpath("//*[@id='tabelaExtrato']/tbody/tr"));
+//			Assert.fail();
+//		} catch (NoSuchElementException e) {
+//
+//		}
+
+        List<WebElement> elementosEncontrados =
+                Driverfactory.getDriver().findElements(By.xpath("//*[@id='tabelaExtrato']/tbody/tr"));
+        Assert.assertEquals(0, elementosEncontrados.size());
+
     }
-
-
 }
